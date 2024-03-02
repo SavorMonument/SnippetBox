d4a45e61439d60dff1b5d2b2f0528dd681c77a57
zzz1 <savormonument@protonmail.com>
2024-03-02 21:22:22 +0200

    centralized logging struct

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,23 +4,41 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
+
+// Application wide dependecies
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
 
 func main() {
 
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
 
+	application := &application{
+		errorLog: log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime),
+		infoLog:  log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile),
+	}
+
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet", showSnippet)
-	mux.HandleFunc("/snippet/create", createSnippet)
+	mux.HandleFunc("/", application.home)
+	mux.HandleFunc("/snippet", application.showSnippet)
+	mux.HandleFunc("/snippet/create", application.createSnippet)
 
 	// Handle static files
 	fileServer := http.FileServer(http.Dir("./ui/static"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Printf("Starting server on %s\n", *addr)
-	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: application.errorLog,
+		Handler:  mux,
+	}
+
+	application.infoLog.Printf("Starting server on %s\n", *addr)
+	err := srv.ListenAndServe()
+	application.errorLog.Fatal(err)
 }
