e6f856578bee0643502edb382dd7f3ca899a56d8
zzz1 <savormonument@protonmail.com>
2024-03-02 22:48:16 +0200

    separate routes

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -18,27 +18,18 @@ func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
 
-	application := &application{
+	app := &application{
 		errorLog: log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime),
 		infoLog:  log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile),
 	}
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", application.home)
-	mux.HandleFunc("/snippet", application.showSnippet)
-	mux.HandleFunc("/snippet/create", application.createSnippet)
-
-	// Handle static files
-	fileServer := http.FileServer(http.Dir("./ui/static"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
 	srv := &http.Server{
 		Addr:     *addr,
-		ErrorLog: application.errorLog,
-		Handler:  mux,
+		ErrorLog: app.errorLog,
+		Handler:  app.routes(),
 	}
 
-	application.infoLog.Printf("Starting server on %s\n", *addr)
+	app.infoLog.Printf("Starting server on %s\n", *addr)
 	err := srv.ListenAndServe()
-	application.errorLog.Fatal(err)
+	app.errorLog.Fatal(err)
 }
