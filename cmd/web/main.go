8abe54095cf552ddd08bd04838cdcab1dc25ae54
zzz1 <savormonument@protonmail.com>
2024-03-02 21:02:07 +0200

    cli arguments

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,23 +1,26 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
 
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
-  // Handle static files
+	// Handle static files
 	fileServer := http.FileServer(http.Dir("./ui/static"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+	log.Printf("Starting server on %s\n", *addr)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }
