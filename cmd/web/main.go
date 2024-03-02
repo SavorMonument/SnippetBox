78cc9277a10dcd5eef9ed0f5428c722547a6227e
zzz1 <savormonument@protonmail.com>
2024-03-03 00:54:46 +0200

    working db connection

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,35 +1,57 @@
 package main
 
 import (
-	"flag"
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql" // New import
 )
 
 // Application wide dependecies
 type application struct {
 	errorLog *log.Logger
 	infoLog  *log.Logger
+	db       *sql.DB
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
 
 func main() {
 
-	addr := flag.String("addr", ":4000", "HTTP network address")
-	flag.Parse()
+	addr := os.Getenv("ADDRESS")
+	dsn := os.Getenv("DSN")
+
+	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime)
+
+	db, err := openDB(dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
 
 	app := &application{
-		errorLog: log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime),
+		errorLog: errorLog,
 		infoLog:  log.New(os.Stderr, "INFO\t", log.Ldate|log.Ltime|log.Lshortfile),
+		db:       db,
 	}
 
 	srv := &http.Server{
-		Addr:     *addr,
+		Addr:     addr,
 		ErrorLog: app.errorLog,
 		Handler:  app.routes(),
 	}
 
-	app.infoLog.Printf("Starting server on %s\n", *addr)
-	err := srv.ListenAndServe()
+	app.infoLog.Printf("Starting server on %s\n", addr)
+	err = srv.ListenAndServe()
 	app.errorLog.Fatal(err)
 }
